b682809d3e4fb67db66b822439924facb304d977
566325 <nethisireesha288@gmail.com>
2023-09-30 23:17:00 +0530

    [Sireesha]:Added Find frequency of words in a sentence

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HashTableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table ");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            hash.GetFrequency("To");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("Fifth index value is :" + hash5);
+        }
+    }
+ }
+    
+    
+    
+    
